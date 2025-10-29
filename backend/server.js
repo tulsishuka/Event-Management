5862744aaf9dbc4a43cb67690102531870a6533e
Tulasi <tulsishuklag@gmail.com>
2025-10-29 15:45:59 +0530

    add env

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,37 @@
+// import { ApolloServer } from "@apollo/server";
+// import { startStandaloneServer } from "@apollo/server/standalone";
+// import { typeDefs, resolvers } from "./schema.js";
+// import { connectDB } from "./db.js";
+
+// async function startServer() {
+//   const db = await connectDB();
+
+//   const server = new ApolloServer({ typeDefs, resolvers });
+
+//   const { url } = await startStandaloneServer(server, {
+//     listen: { port: 4000 },
+//     context: async () => ({ db }),
+//   });
+
+//   console.log(`Server running at ${url}`);
+// }
+
+// startServer();
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs, resolvers } from "./schema.js";
 import { connectDB } from "./db.js";
 
-async function startServer() {
-  const db = await connectDB();
+const startServer = async () => {
+  await connectDB();
 
   const server = new ApolloServer({ typeDefs, resolvers });
 
   const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-    context: async () => ({ db }),
+    listen: { port: process.env.PORT || 3000 },
   });
 
-  console.log(`Server running at ${url}`);
-}
+  console.log(`🚀 Server ready at ${url}`);
+};
 
 startServer();
