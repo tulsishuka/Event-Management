35a1714feec36de87fcd33f497ff629337704f25
Tulasi Shukla <tulsishuklag@gmail.com>
2025-11-02 15:40:53 +0530

    Update apolloClient.js

--- frontend/lib/apolloClient.js
+++ frontend/lib/apolloClient.js
@@ -4,7 +4,8 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:4000/graphql", 
+    // uri: "http://localhost:4000/graphql", 
+     uri: "https://event-management-2-e1to.onrender.com/graphql", 
   }),
   cache: new InMemoryCache(),
 });
